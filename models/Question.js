6d36d857c2684b8de64017f041b4610a0b41afcf
NIKHIL-TEJA341 <nikhilteja341@gmail.com>
2026-01-07 18:36:35 +0530

    Deploy Backend

---
+++ models/Question.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    solutionLink: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Question", questionSchema);
